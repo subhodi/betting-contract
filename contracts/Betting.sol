d5d04c31c63e0abba58b773592153a9ede4bd77e
Subhod I <subhodpoojary@gmail.com>
2017-11-07 12:25:38 +0530

    betting contract add

---
+++ contracts/Betting.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.4.15; // Compiler version
+
+contract Betting {
+    
+    address private owner;
+    uint initialBalance = 0;
+    mapping( bytes32 => uint) balance;
+    
+    modifier onlyOwner() {
+        require( owner == msg.sender);
+        _;
+    }
+    
+    modifier notExist(bytes32 _username) {
+        require( balance[_username] == 0);
+        _;
+    }
+    
+    function Betting(uint _initialBalance) public {
+        owner = msg.sender;
+        initialBalance = _initialBalance;
+    }
+    
+    function register(bytes32 _username) public onlyOwner notExist(_username) {
+        balance[_username] = initialBalance;
+    }
+    
+    function getBalance(bytes32 username) public constant returns(uint) {
+        return balance[username];
+    }
+    
+    
+    
+}
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,8 @@
 var ConvertLib = artifacts.require("./ConvertLib.sol");
-var MetaCoin = artifacts.require("./MetaCoin.sol");
+var Betting = artifacts.require("./Betting.sol")
 
 module.exports = function(deployer) {
   deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+  deployer.link(ConvertLib, Betting);
+  deployer.deploy(Betting);
 };
