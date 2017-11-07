912ab066acd706507c841e7db7c8d10c9694a167
Subhod I <subhodpoojary@gmail.com>
2017-11-07 14:24:11 +0530

    Contract function declarations | Bet contract

--- contracts/Bet.sol
+++ contracts/Bet.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.15; 
 
-contract Betting {
+contract Bet {
     
     address private owner;
     uint public initialBalance = 0;
@@ -29,17 +29,6 @@ contract Betting {
         return balance[username];
     }
     
-    function getLeaderboard(bytes32 _username) public constant returns(bytes32[5], uint[5]) {
-        bytes32[5] memory usernames;
-        uint[5] memory score;
-        usernames = [bytes32('user1'),bytes32('user2'),bytes32('user3'),bytes32('user4'), _username];
-        score = [uint(170), uint(130), uint(110), uint(90), uint(30)];
-        return (usernames, score);
-    }
-    
-    function placeBet(bytes32 _username, uint _amount) public {
-        balance[_username] -= _amount;
-    }
     
     
 }
--- test/TestBetting.sol
+++ test/TestBetting.sol
@@ -7,11 +7,11 @@ import "../contracts/Betting.sol";
 contract TestBetting {
 
   function testInitialBalanceWithNewBetting() {
-     Betting bet = new Betting(1000);
+     Betting betting = new Betting(1000);
 
      uint expected = 1000;
 
-     Assert.equal(bet.initialBalance(), expected, "New contract deployment failed: Initial balance is different");
+     Assert.equal(betting.initialBalance(), expected, "New contract deployment failed: Initial balance is different");
   }
 
 }
