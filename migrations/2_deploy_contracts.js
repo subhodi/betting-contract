a51ac98d9c7b7c9664da9743db40d3aa18e2c635
Aaron Colaco <aaron_colaco@persistent.co.in>
2017-11-14 13:58:35 +0530

    Fix linting errors

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,8 @@
-var Betting = artifacts.require("./Betting.sol")
-var Bet = artifacts.require("./Bet.sol")
-module.exports = function(deployer) {
-  deployer.deploy(Betting,100).then(function(){
-    return deployer.deploy(Bet,Betting.address);
+var Betting = artifacts.require("./Betting.sol");
+var Bet = artifacts.require("./Bet.sol");
+
+module.exports = function (deployer) {
+  deployer.deploy(Betting, 100).then(function () {
+    return deployer.deploy(Bet, Betting.address);
   });
 };
